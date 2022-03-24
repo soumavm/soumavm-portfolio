abb7ff81574e0645a2a9e9e40be6bfb9ca13c0e9
soumavm <soumavm@gmail.com>
2022-03-24 17:46:38 -0600

    Added colour palette

--- src/theme/variables/colours.js
+++ src/theme/variables/colours.js
@@ -1,5 +1,10 @@
 const colours = {
-
+    blue: "#1a73e8",
+    darkBlue: "#185abc",
+    blackText: "#202124",
+    lightGrayText: "#5f6368",
+    darkGrayText: "#202124",
+    grayHover: "#5f6368"
 }
 
 export default colours
